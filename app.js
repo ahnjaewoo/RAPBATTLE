45df0c2653af1425ce9fa2366888b453462af4a1
jinyo <solomoj94@hanmail.net>
2017-07-27 21:43:10 +0900

    passport

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cors = require('cors');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -22,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.all('/', index);
+app.all('/users*', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
